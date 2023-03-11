9482120c34ce6479ad1e547c0c68a479e04c49c4
kimjh <kimjh@xxblue.com>
2023-03-11 18:09:01 +0900

    * spring security jdbc user detail manager

--- src/main/java/com/example/spring_basic/taco/security/SecurityConfig.java
+++ src/main/java/com/example/spring_basic/taco/security/SecurityConfig.java
@@ -1,14 +1,15 @@
 package com.example.spring_basic.taco.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
 @Configuration
@@ -27,9 +28,9 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
-        UserDetails user2 = User.withUsername("user2").password("{noop}password2").roles("USER").build();
-        return new InMemoryUserDetailsManager(user1, user2);
+    public UserDetailsService userDetailsService(@Autowired DataSource dataSource) {
+//        UserDetails user1 = User.withUsername("user1").password("{noop}password1").roles("USER").build();
+//        UserDetails user2 = User.withUsername("user2").password("{noop}password2").roles("USER").build();
+        return new JdbcUserDetailsManager(dataSource);
     }
 }
